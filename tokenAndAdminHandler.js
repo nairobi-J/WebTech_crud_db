cba2d4b338f7c9ae822c9c78648a348bd03d8e9f
nusrat jahan jerin <107641424+nairobi-J@users.noreply.github.com>
2024-05-11 23:01:50 +0600

    tokenAndAdminHandler.js

--- tokenAndAdminHandler.js
+++ tokenAndAdminHandler.js
@@ -1,21 +1,25 @@
 const jwt = require('jsonwebtoken');
-
+//json webtoken should be stored with bearer in auth
+//login verify kore.
 const verifyToken = (req, res, next) => {
     const { authorization } = req.headers;
     try {
+        //access_token_amr nijer icche moto kisu dite parbo
+        
         const token = authorization.split(' ')[1];
-        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        const { email, username, role } = decoded;
-        req.email = email;
+        const decode = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        
+        const { email, username, role } = decode; req.email = email;
         req.name = username;
         req.role = role;
+        //forward next url
         next();
     } catch {
         next('Authentication Failure');
     }
 }
 
-// authMiddleware.js
+//if amin or not for deleting task
 const isAdmin = (req, res, next) => {
     if (req.user.role !== 'admin') {
         return res.status(403).json({ error: 'authorization failed !' });
