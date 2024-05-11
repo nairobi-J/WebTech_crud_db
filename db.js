d3f123e941b2ca0c7b388a91e69abc6edbce8dfc
nusrat jahan jerin <107641424+nairobi-J@users.noreply.github.com>
2024-05-11 21:36:20 +0600

    db.js

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql');
-
+//creating connection with the database
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
   database: 'task_db'
 });
-
+//catching error connecting database
 db.connect((err) => {
   if (err) {
     console.error('Error connecting to database:', err);
